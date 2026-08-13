da8386ab1130977029fb8cdb4e7660cfe33f3020
GabrielHuanuco <167567071+GabrielHuanuco@users.noreply.github.com>
2026-08-12 22:32:44 -0500

    Update libreria_funciones.py

--- libreria_funciones.py
+++ libreria_funciones.py
@@ -213,3 +213,7 @@ def disponibilidad_servicio(total_horas, horas_caida, mantenimientos_programados
 
     return max(disponibilidad_ajustada, 0)
 
+def interes_simple(monto, interes, tiempo_meses):
+  interes = monto * interes * (tiempo_meses/12)
+  return interes
+
